0976bac718117a6d55045350f5fff03fa30d5a56
xiaoyulyu999 <info@bloomora.net>
2025-11-08 16:59:23 +0000

    home

--- main.cpp
+++ main.cpp
@@ -1,16 +1,13 @@
-#include <iostream>
+#include "raylib.h"
 
-// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 int main() {
-    // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the <b>lang</b> variable name to see how CLion can help you rename it.
-    auto lang = "C++";
-    std::cout << "Hello and welcome to " << lang << "!\n";
-
-    for (int i = 1; i <= 5; i++) {
-        // TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        std::cout << "i = " << i << std::endl;
+    InitWindow(800, 450, "Hello Raylib (C++)");
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        DrawText("Hello from Raylib + C++!", 300, 200, 20, BLACK);
+        EndDrawing();
     }
-//try
+    CloseWindow();
     return 0;
-
 }
