3a71c556d3a9bf4349a92c03dbbb5a4c2c5906ca
xiaoyulyu999 <info@bloomora.net>
2025-11-08 15:58:32 +0000

    home

--- main.cpp
+++ main.cpp
@@ -10,7 +10,5 @@ int main() {
         // TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
         std::cout << "i = " << i << std::endl;
     }
-
     return 0;
-    // TIP See CLion help at <a href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>. Also, you can try interactive lessons for CLion by selecting 'Help | Learn IDE Features' from the main menu.
 }
